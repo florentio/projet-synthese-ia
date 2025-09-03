34d00b50018c80f1c6985869870102ca8a720589
florentio <desouza.florentio@gmail.com>
2025-09-03 14:23:27 -0400

    ci

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-import os
-print("Current directory:", os.getcwd())
-for root, dirs, files in os.walk('.'):
-    if 'telco_churn.csv' in files:
-        print(f"File found at: {os.path.join(root, 'telco_churn.csv')}")
